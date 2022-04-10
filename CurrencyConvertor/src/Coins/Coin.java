e7aa92a09770063918f87b4e0e080f89a9c4ff2d
Igorsh1987 <96612823+Igorsh1987@users.noreply.github.com>
2022-04-10 17:20:05 +0300

    Delete CurrencyConvertor/src directory

--- CurrencyConvertor/src/Coins/Coin.java
+++
@@ -1,9 +0,0 @@
-package Coins;
-
-public abstract class Coin implements ICalculate {
-    public double calculate(double value) {
-        return value;
-    }
-
-    abstract double getValue();
-}
--- CurrencyConvertor/src/Coins/ICalculate.java
+++
@@ -1,7 +0,0 @@
-package Coins;
-//Create an Interface named ICalcualte
-public interface ICalculate {
-    //method calculate() which gets a double argument
-    //and returns a double value
-    double calculate(double value);
-}
